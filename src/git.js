ba4957027ce2d1576fd3b879c488c7f2e00d7c7c
gbenson <geoff.benson@asktia.com>
2022-06-02 17:44:06 -0700

    Remove some extraneous error catching

--- src/git.js
+++ src/git.js
@@ -1,15 +1,10 @@
 const simpleGit = require("simple-git");
 
 async function gitClone(url, wikiPath) {
-  try {
     return simpleGit().clone(url, wikiPath);
-  } catch (e) {
-    console.error(e);
-  }
 }
 
 async function gitUpdate(wikiPath) {
-  try {
     return simpleGit(wikiPath)
         .status(['-v', '-v'])
         .addConfig("user.name", "Coverage Diff Action")
@@ -18,9 +13,6 @@ async function gitUpdate(wikiPath) {
         .status(['-v', '-v'])
         .commit("Update coverage badge")
         .push();
-  } catch (e) {
-    console.error(e);
-  }
 }
 
-module.exports = { gitClone, gitUpdate };
+module.exports = {gitClone, gitUpdate};
